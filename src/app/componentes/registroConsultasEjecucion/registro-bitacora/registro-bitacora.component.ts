c16056e5db667690cebc1079ef0fc9e87643e6a9
unknown <guillegmo@hotmail.com>
2022-01-20 21:08:22 -0500

    Actualiza Bitacora

--- src/app/componentes/registroConsultasEjecucion/registro-bitacora/registro-bitacora.component.ts
+++ src/app/componentes/registroConsultasEjecucion/registro-bitacora/registro-bitacora.component.ts
@@ -21,7 +21,7 @@ export class RegistroBitacoraComponent implements OnInit {
 
   actividades: any =[];
 
-  dias = [] = [1,2,3,4,5,6,7,8,9,0,11,12];
+  dias = [] = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24 ];
 
   actividadPrincipal: any = [];
   actividadSecundaria: any = [];
